aa0b6c274d50ee3639b73431cc0e4be37ed03e37
Sean Dorris <seanc.dorris@gmail.com>
2023-02-09 21:04:31 -0600

    saving changes

--- main.js
+++ main.js
@@ -8,24 +8,34 @@ const repos = [];
 function renderRepoForm () {
   console.log("testing");
 
-//get DOM elements
-const repoAddDiv = document.querySelector("#repoFormDiv");
+  //get DOM elements
+  const repoAddDiv = document.querySelector("#repoFormDiv");
 
-repoAddDiv.innerHTML = 
-  `<form id="repoform" class="row g-3">
-  <div class="col-12">
-    <label for="repoName" class="form-label">Repository Name<span id="required">*</span></label>
-    <input type="text" class="form-control" id="inputAddress">
-  </div>
-  
-  <div class="col-12">
-    <label for="repoDescription" class="form-label">Description</label><label class="form-label">(optional)</label>
-    <textarea class="form-control" id="descInput" rows="3"></textarea>
-  </div>
+  repoAddDiv.innerHTML = 
+    `<form id="repoform" class="row g-3">
+    <div class="col-12">
+      <label for="repoName" class="form-label">Repository Name<span id="required">*</span></label>
+      <input type="text" class="form-control" id="inputAddress">
+    </div>
     
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Create Repository</button>
-  </div>
-</form>`
-}
+    <div class="col-12">
+      <label for="repoDescription" class="form-label">Description</label><label class="form-label">(optional)</label>
+      <textarea class="form-control" id="descInput" rows="3"></textarea>
+    </div>
+      
+    <div class="col-12">
+      <button id="repoSubmitBtn" type="submit" class="btn btn-primary">Create Repository</button>
+    </div>
+  </form>`
+};
+
 renderRepoForm()
+
+//add repo card function
+function addRepo() {
+  console.log("button tester!");
+  const repoCardDiv = document.querySelector("#repoCards")
+
+
+}
+document.querySelector("#repoSubmitBtn").addEventListener("click", console.log("idiot"));
