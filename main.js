d4c191c39bd5a5d096eaa934e3c882b7459183eb
Sean Dorris <seanc.dorris@gmail.com>
2023-02-09 19:46:28 -0600

    added code for issues in branch name

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("hex clan group project")
+
+//variable to house repo data objects
+const repos = [];
+
+
+//render repo form to DOM
+function renderRepoForm () {
+  console.log("testing");
+
+//get DOM elements
+const repoAddDiv = document.querySelector("#repoFormDiv");
+
+repoAddDiv.innerHTML = 
+  `<form id="repoform" class="row g-3">
+  <div class="col-12">
+    <label for="repoName" class="form-label">Repository Name<span id="required">*</span></label>
+    <input type="text" class="form-control" id="inputAddress">
+  </div>
+  
+  <div class="col-12">
+    <label for="repoDescription" class="form-label">Description</label><label class="form-label">(optional)</label>
+    <textarea class="form-control" id="descInput" rows="3"></textarea>
+  </div>
+    
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Create Repository</button>
+  </div>
+</form>`
+}
+renderRepoForm()
