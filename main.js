40ac943970ee8ee2d2e853b1544679f4f30f35f0
commando954 <bill.y@live.com>
2023-02-09 19:46:33 -0600

    saved progress

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("hex clan group project")
+
+
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+/* const cardsOnDom = (array) => {
+  let domString = ""
+  for (const pinned of array) {
+    domString +=
+    `<div class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>`
+  }
+}
+*/
