bc3b265ef02c16ceffc4a7c885f671b11da0e299
Steve Butler <butlersteve.c@gmail.com>
2023-02-12 17:34:25 -0600

    steve changed stuff to try to fix navbar and other issues

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,4 @@
-export const navbar = `<div class="container-fluid">
+export const mainMenu = `<div class="container-fluid">
 <a class="navbar-brand" href="../index.html">
   <svg
     xmlns="http://www.w3.org/2000/svg"
